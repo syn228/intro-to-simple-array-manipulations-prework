c800e813ee5ba15be86ffc811ca6cb3a1046b2f4
Aaron Na <syn228@nyu.edu>
2018-04-10 21:03:51 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,4 +11,24 @@ def using_pop(array)
 end
 
 def pop_with_args(array)
+  array.pop(2)
+end
   
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array1, array2)
+  array1 << array2
+end
+
+def using_insert(array, element)
+  
+
+
+
+
