877e6ea149696e084ba11347c847b3d4c8a29cfa
Aaron Na <syn228@nyu.edu>
2018-04-10 21:06:44 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -35,6 +35,15 @@ def using_uniq(array)
 end
 
 def using_flatten(array)
-  
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete[integer]
+end
 
 
